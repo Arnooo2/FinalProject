6ed3d5daa46d01b7cfeb991a601453f21aab8d25
Arnooo2 <arno0136@gmail.com>
2025-11-12 22:58:14 +0200

    v1

--- FinalProject/FinalProject.cpp
+++ FinalProject/FinalProject.cpp
@@ -1,5 +1,44 @@
 #include <iostream>
+#include <string>
 
+using namespace std;
+
+struct Transaction {
+    string type;
+    double sum;
+    string date;
+};
+
+class Account {
+private:
+    double bal;
+
+public:
+    explicit Account(double b = 0.0) {
+        bal = b;
+    }
+
+    Account(const Account& other) {
+        bal = other.bal;
+    }
+
+    void add(double s) {
+        bal += s;
+    }
+
+    void take(double s) {
+        if (s <= bal) bal -= s;
+        else cout << "Not enough money" << endl;
+    }
+
+    double getBal() const {
+        return bal;
+    }
+
+    void show() const {
+        cout << "Balance: " << bal << endl;
+    }
+};
 int main()
 {
 	//tyt pysto
